3d0ef379e4964f9ecfa9b056d92d6f70817579c0
Milos Vasic <milos85vasic@gmail.com>
2020-08-24 19:21:12 +0200

    Initial classes.

---
+++ Factory/src/main/kotlin/net/milosvasic/factory/caching/BuildInfo.kt
@@ -0,0 +1,8 @@
+package net.milosvasic.factory.caching
+
+object BuildInfo {
+
+    const val NAME = "Caching-Proxy-Server-Factory"
+    const val VERSION = "1.0.0"
+    const val VERSION_CODE = (100 * 1000) + 0
+}
---
+++ Factory/src/main/kotlin/net/milosvasic/factory/caching/main.kt
@@ -0,0 +1,24 @@
+@file:JvmName("Launcher")
+
+package net.milosvasic.factory.caching
+
+
+import net.milosvasic.factory.fail
+import net.milosvasic.factory.log
+import net.milosvasic.factory.tag
+import net.milosvasic.logger.ConsoleLogger
+import net.milosvasic.logger.FilesystemLogger
+import java.io.File
+
+fun main(args: Array<String>) {
+
+    initLogging()
+    log.v("Work in progress")
+}
+
+private fun initLogging() {
+    tag = BuildInfo.NAME
+    val console = ConsoleLogger()
+    val filesystem = FilesystemLogger(File("."))
+    compositeLogger.addLoggers(console, filesystem)
+}

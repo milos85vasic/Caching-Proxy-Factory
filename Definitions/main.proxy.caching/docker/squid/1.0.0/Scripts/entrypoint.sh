af37d89c4b8ec2f2b257130d61caa1af32882b0c
Milos Vasic <milos85vasic@gmail.com>
2020-12-24 15:45:23 +0100

    MSF-233 - Working on init scripts.

--- Definitions/main.proxy.caching/docker/squid/1.0.0/Scripts/entrypoint.sh
+++ Definitions/main.proxy.caching/docker/squid/1.0.0/Scripts/entrypoint.sh
@@ -3,15 +3,16 @@
 set -e
 
 SQUID_CONF=/etc/squid/squid.conf
+SQUID_CACHE_DIR=/var/cache/squid
 
 echo "Checking cache..."
 if ! test -e "$SQUID_CACHE_DIR"/00; then
 
-    echo "No existing cache found. Initializing new cache..."
-    squid -N -f ${SQUID_CONF} -z
+  echo "No existing cache found. Initializing new cache..."
+  squid -N -f "$SQUID_CONF" -z
 else
-    echo "Found existing cache at ${SQUID_CACHE_DIR}"
+  echo "Found existing cache at $SQUID_CACHE_DIR"
 fi
 
 echo "Starting Squid..."
-squid -f ${SQUID_CONF} -NYCd 1
+squid -f "$SQUID_CONF" -NYCd 1

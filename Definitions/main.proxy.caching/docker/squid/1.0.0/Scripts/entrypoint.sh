da0da266666bc2cb00ead236677df473ba7f7a57
Milos Vasic <milos85vasic@gmail.com>
2021-01-07 12:10:08 +0100

    MSF-395 - Check user accounts.

--- Definitions/main.proxy.caching/docker/squid/1.0.0/Scripts/entrypoint.sh
+++ Definitions/main.proxy.caching/docker/squid/1.0.0/Scripts/entrypoint.sh
@@ -11,8 +11,17 @@ if ! test -e "$SQUID_CACHE_DIR"/00; then
   echo "No existing cache found. Initializing new cache"
   squid -N -f "$SQUID_CONF" -z
 else
+
   echo "Found existing cache at $SQUID_CACHE_DIR"
 fi
 
-echo "Starting Squid"
-squid -f "$SQUID_CONF" -NYCd 1
+echo "Checking user accounts"
+if /usr/lib64/squid/basic_ncsa_auth /etc/squid/passwords/accounts; then
+
+  echo "Starting Squid"
+  squid -f "$SQUID_CONF" -NYCd 1
+else
+
+  echo "ERROR: checking user accounts failed"
+  exit 1
+fi
